2a7b862102ca272f94d89c0febda6ac2ea6d8dc0
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-09-09 11:09:57 -0600

    Add functionality for add task button

--- imports/tinker.py
+++ imports/tinker.py
@@ -4,11 +4,18 @@ import tkinter .messagebox
 root = tkinter.Tk()
 root.title("To-Do List")
 
+def add_task():
+  task = entry_task.get()
+  listbox_tasks.insert(tkinter.END, task)
+
 #Create GUI
 listbox_tasks = tkinter.Listbox(root, height=3, width=50)
 listbox_tasks.pack()
 
 entry_task = tkinter.Entry(root, width=50)
 entry_task.pack()
+
+button_add_task = tkinter.Button(root, text="Add task", width=48, command=add_task)
+button_add_task.pack()
 
 root.mainloop()
